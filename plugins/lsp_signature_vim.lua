ea32273383dec1cbc3c58a39664c25422421c54b
Alex Veden <i@alexveden.com>
2023-05-20 10:31:08 +0400

    lsp signature help + toggle key was fixed

---
+++ plugins/lsp_signature_vim.lua
@@ -0,0 +1,14 @@
+return {
+    {
+        "ray-x/lsp_signature.nvim",
+        event = "BufRead",
+        config = function(_, _)
+            require("lsp_signature").setup({
+                bind = true,
+                hint_prefix = "󱧢 ", --""🐼 ", -- Panda for parameter, NOTE: for the terminal not support emoji, might crash
+                toggle_key = '<C-q>', -- toggle signature on and off in insert mode,  e.g. toggle_key = '<M-x>'
+                toggle_key_flip_floatwin_setting = true, -- true: toggle float setting after toggle key pressed
+            })
+        end,
+    },
+}
