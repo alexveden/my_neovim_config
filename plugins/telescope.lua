f41ab6c1c467f31d246b27528c7e33a16439d93e
Alex Veden <i@alexveden.com>
2023-05-20 10:59:13 +0400

    telescope formatting and mappings

--- plugins/telescope.lua
+++ plugins/telescope.lua
@@ -6,7 +6,25 @@ return {
     "debugloop/telescope-undo.nvim",
   },
   config = function()
+    local telescope_actions = require('telescope.actions')
+
     require("telescope").setup({
+        defaults = {
+            dynamic_preview_title = true,
+            --path_display = { shorten = { len = 2 } }
+            path_display = { "smart" },
+            mappings = {
+                i = {
+                    ["<PageUp"] = false,
+                    ["<PageDown"] = false,
+                    ["<C-Up>"] = telescope_actions.cycle_history_prev,
+                    ["<C-Down>"] = telescope_actions.cycle_history_next,
+
+                    ["<C-PageUp>"] = telescope_actions.preview_scrolling_up,
+                    ["<C-PageDown>"] = telescope_actions.preview_scrolling_down,
+                },
+            },
+        },
       extensions = {
         undo = {
           -- telescope-undo.nvim config, see below
